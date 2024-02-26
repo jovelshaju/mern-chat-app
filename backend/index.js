a61de12bdc9d0f5772369e086559ac9156995713
Jovel Shaju <jovelshaju@gmail.com>
2024-02-26 20:26:52 +0530

    Connected MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,22 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
+const connectDB = require("./config/mongoDb");
+
+connectDB();
 
 const app = express();
 
 //Constants
-const PORT  = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 //Routes
-app.get('/', (req, res) => {
-    res.json({
-        message: "Welcome to ChatApp with MERN!"
-    });
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to ChatApp with MERN!",
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening to http://localhost:${PORT}`);
+  console.log(`Listening to http://localhost:${PORT}`);
 });
